a5e803c421dbaae01388dcb1f6393955d38d8df6
ronan-jesus <ronan.jesus@bol.com.br>
2020-12-02 17:53:59 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+DEBUG = True
+
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/User/Google Drive/SETORIZAPY/app/database/db_setorizapy_flask.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SECRET_KEY = 'UM-NOME-BEM-SEGURO'
+
+GOOGLEMAPS_KEY = "AIzaSyAsz3mhSUQ65t3NhwB6NJ5dhbKJt9haGdQ"
---
+++ run.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 13:43:32 2020
+
+@author: User
+"""
+from app import manager
+
+if __name__ == "__main__":
+    manager.run()
